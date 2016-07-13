aeb2792c1e63c6ab1afe6a6594dd8ac4ce0d2d73
Min Lee <MinMac@Mins-MacBook-Pro.local>
2016-07-13 14:30:22 -0400

    added login

--- www/js/app.js
+++ www/js/app.js
@@ -1,64 +1,72 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.billController', 'angularMoment'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.billController', 'angularMoment', 'loginController', 'loginService'])
 
 .run(function($ionicPlatform) {
-    
-   
-    $ionicPlatform.ready(function() {
 
-      
-      
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        //cordova.plugins.Keyboard.disableScroll(true);
-      }
-      
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+
+        $ionicPlatform.ready(function() {
+
+
+
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                //cordova.plugins.Keyboard.disableScroll(true);
+            }
+
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+            .state('tab.inventory', {
+                url: '/inventory',
+                views: {
+                    'tab-inventory': {
+                        templateUrl: '/templates/tab-inventory.html',
+                        controller: 'InventoryCtrl'
+                    }
+                }
+            })
+            .state('tab.opentabs', {
+                url: '/opentabs',
+                views: {
+                    'tab-opentabs': {
+                        templateUrl: 'templates/tab-opentabs.html',
+                        controller: 'OpenTabCtrl'
+                    }
+                }
+            })
+            .state('tab.opentab-bill', {
+                url: '/opentabs/:tabID',
+                views: {
+                    'tab-opentabs': {
+                        templateUrl: 'templates/opentabs-bill.html',
+                        controller: 'BillCtrl'
+                    }
+                }
+            })
+            .state('tab.specials', {
+                url: '/specials',
+                views: {
+                    'tab-specials': {
+                        templateUrl: '/templates/tab-specials.html',
+                        controller: 'SpecialsCtrl'
+                    }
+                }
+            })
+            .state('login', {
+                url: '/login',
+                
+                    templateUrl: 'templates/login.html',
+                    controller: 'LoginCtrl'
+
+
+            })
+        $urlRouterProvider.otherwise('/login');
     });
-  })
-  .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
-      .state('tab.inventory', {
-        url: '/inventory',
-        views: {
-          'tab-inventory': {
-            templateUrl: '/templates/tab-inventory.html',
-            controller: 'InventoryCtrl'
-          }
-        }
-      })
-      .state('tab.opentabs', {
-        url: '/opentabs',
-        views: {
-          'tab-opentabs': {
-            templateUrl: 'templates/tab-opentabs.html',
-            controller: 'OpenTabCtrl'
-          }
-        }
-      })
-      .state('tab.opentab-bill', {
-        url: '/opentabs/:tabID',
-        views: {
-          'tab-opentabs': {
-            templateUrl: 'templates/opentabs-bill.html',
-            controller: 'BillCtrl'
-          }
-        }
-      })
-      .state('tab.specials', {
-        url: '/specials',
-        views: {
-          'tab-specials': {
-            templateUrl: '/templates/tab-specials.html',
-            controller: 'SpecialsCtrl'
-          }
-        }
-      })
-    $urlRouterProvider.otherwise('/tab/opentabs');
-  });
---
+++ www/js/loginController.js
@@ -0,0 +1,17 @@
+angular.module('loginController', [])
+
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {};
+
+    $scope.login = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            console.log("Logged in");
+            $state.go('tab.opentabs');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+})
